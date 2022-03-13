ee162da688766096c95414ffd5ba474d566dade3
IAteYourKookie <loorenachan@gmail.com>
2022-03-12 22:26:41 -0300

    adicao javascript

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+/*
+quando clicar no pokemon da listagem o cartao dele e escondido e o cartao do pokemon
+selecionado e mostrado
+
+variavel const (constante) => o valor nao pode ser alterado
+variavel let => valor pode ser alterado
+
+precisaremos trabalhar com dois elementos
+
+vamos precisar trabalhar com o evento de clique
+feito pelo usuario na listagem de pokemon
+
+- remover a classe aberto
+- ao clicar em um pokemon da listagem pegamos o id desse
+pokemon para saber qual cartao mostrar
+*/
+
+//precisamos criar duas variaveis no JS pra trabalhar com os elementos da tela
+
+const listaSelacaoPokemons = document.querySelectorAll()
+
+console.log()
